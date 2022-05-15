40053c20a1f12bf14c7f13e941f7ebfa08a69a93
Bloo00 <kabin.yangg@gmail.com>
2022-05-15 13:48:13 -0700

    cleaning

--- src/container/Home/index.jsx
+++ src/container/Home/index.jsx
@@ -13,25 +13,7 @@ export default class Home extends Component{
         return(
             <>
             <Navbar/>
-            <div className="meat">
-                <div className="upper">
-                    <div className="youtube">
-                    <iframe width="560" height="315" src="https://www.youtube-nocookie.com/embed/9U8pVtP3cos?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-                    </div>
-                    <div className="about">
-                        <div className="aboutTitle">Conveyors Everywhere!</div>
-                        <div className="summary">
-
-                        </div>
-                        <div className="youtubeLink">No desc cause MONICA IS UN CREATIVE!!!!!{`>`}:0</div>
-                    </div>
-                </div>
-                <br/>
-
-                <div className="lower">
-                    <Youtube/>
-                </div>
-            </div>
+            
             </>
         )
     }
